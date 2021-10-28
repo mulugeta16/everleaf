9df9223486a93dc2c026d8c6815bb954670d0ded
mulugeta16 <mulugeta2030@gmail.com>
2021-10-28 16:16:08 +0900

    internationalization and system spec finished

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -11,7 +11,6 @@ RSpec.describe 'Task management function', type: :system do
       end
     end
   end
-  describe 'Task management function', type: :system do
     describe 'List display function' do
       context 'When transitioning to the list screen' do
         it 'The created task list is displayed' do
@@ -31,5 +30,11 @@ RSpec.describe 'Task management function', type: :system do
          expect(page).to have_content 'task_name1'
        end
      end
+     context 'When the tasks are arranged in descending order of creation date and time' do
+          it 'A new task is displayed at the top' do
+            task = Task.create(name: 'task1', description: 'description1')
+            visit tasks_path
+            assert Task.all.order(created_at: "desc")
+          end
+        end
   end
-end
