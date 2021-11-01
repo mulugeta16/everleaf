f61cde1e194e5c651f0509f26dfb3f2f4f5a6d02
mulugeta16 <mulugeta2030@gmail.com>
2021-11-01 17:58:05 +0900

    step3 first draft

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,20 +1,34 @@
 class TasksController < ApplicationController
   before_action :set_task, only: %i[ show edit update destroy ]
-
-  # GET /tasks or /tasks.json
   def index
-    @tasks = Task.all
-    @tasks = @tasks.order('created_at DESC')
-  end
-
+     @tasks = Task.all
+      if params[:sort_expired]
+       @tasks = @tasks.order('deadline DESC')
+   elsif params[:name].present?
+     if params[:status].present?
+      @tasks = @tasks.name_search(params[:name]).status_search(params[:status])
+    else
+      @tasks = @tasks.name_search(params[:name])
+    end
+  elsif params[:status].present?
+      @tasks = @tasks.status_search(params[:status])
+  elsif params[:label_id].present?
+      @tasks = @tasks.label_search(params[:label_id])
+  elsif params[:sort_priority]
+      @tasks = @tasks.priority_ordered.page params[:page]
+  else
+      @tasks = @tasks.order('created_at DESC')
+      @tasks = @tasks.order(created_at: :desc)
+    end
+end
   # GET /tasks/1 or /tasks/1.json
-  def show
+ def show
   end
 
   # GET /tasks/new
   def new
-    @task = Task.new
-  end
+     @task = Task.new
+   end
 
   # GET /tasks/1/edit
   def edit
@@ -23,23 +37,22 @@ class TasksController < ApplicationController
   # POST /tasks or /tasks.json
   def create
     @task = Task.new(task_params)
-
-    respond_to do |format|
-      if @task.save
-        format.html { redirect_to @task, notice: "Task was successfully created." }
-        format.json { render :show, status: :created, location: @task }
+   if params[:back]
+  render :new
+  else
+    if @task.save
+    redirect_to tasks_path, notice: "The task was successfully created"
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
+         render :new
       end
-    end
-  end
+     end
+   end
 
   # PATCH/PUT /tasks/1 or /tasks/1.json
   def update
     respond_to do |format|
       if @task.update(task_params)
-        format.html { redirect_to @task, notice: "Task was successfully updated." }
+        format.html { redirect_to @task, notice: "The task was successfully updated." }
         format.json { render :show, status: :ok, location: @task }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,19 +65,17 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     respond_to do |format|
-      format.html { redirect_to tasks_url, notice: "Task was successfully destroyed." }
+      format.html { redirect_to tasks_url, notice: "The task was successfully deleted." }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_task
-      @task = Task.find(params[:id])
-    end
+  def set_task
+    @task = current_user.tasks.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def task_params
-      params.require(:task).permit(:name, :description)
-    end
-end
+	def task_params
+		params.require(:task).permit(:name, :description, :status, :priority, :deadline)
+	end
+  end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,33 @@
 class Task < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true , length: { in: 1..200 }
-end
+
+  enum priority: [:low, :medium, :high]
+
+  	scope :name_search, -> (query) {where("name LIKE ?", "%#{query}%")}
+  	def name_search(query)
+  	  where("name LIKE ?", "%#{query}%")
+  	end
+
+  	scope :status_search, -> (query) {where(status: query)}
+  	def status_search(query)
+  	  where(status: query)
+  	end
+    scope :user_task_list, -> (query) {where(user_id: query)}
+    def user_task_list(query)
+      where(user_id: query)
+    end
+
+    scope :label_search, -> (query) {
+		@ids = Labelling.where(label_id: query).pluck(:task_id)
+		where(id: @ids)}
+
+  	scope :priority_ordered, -> {order("
+  	    CASE tasks.priority
+  	    WHEN 'high' THEN 'a'
+  	    WHEN 'medium' THEN 'b'
+  	    WHEN 'low' THEN 'c'
+  	    ELSE 'z'
+  	    END ASC,
+  	    id DESC" )}
+ end
